f34d73959cef0c7508543b914347cf4ee0a60408
Vladislav Makarenko <makarenko@mpdl.mpg.de>
2014-08-12 21:47:01 +0200

    the dummy class to register correct multipart forms handling by tomcat

---
+++ src/main/java/de/mpg/mpdl/api/swc/MyApplication.java
@@ -0,0 +1,23 @@
+package de.mpg.mpdl.api.swc;
+
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Portable JAX-RS application.
+ *
+ */
+public class MyApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        // register resources and features
+        classes.add(MultiPartFeature.class);
+        classes.add(RestApi.class);
+        //classes.add(LoggingFilter.class);
+        return classes;
+    }
+}
