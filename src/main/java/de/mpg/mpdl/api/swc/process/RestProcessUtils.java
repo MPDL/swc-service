c2cdd53cdbd6ee1333e47c9f22bac04c37d949c1
Bastien Saquet <saquet@mpdl.mpg.de>
2014-08-13 15:32:22 +0200

    add use firefox to screenshot

--- src/main/java/de/mpg/mpdl/api/swc/process/RestProcessUtils.java
+++ src/main/java/de/mpg/mpdl/api/swc/process/RestProcessUtils.java
@@ -34,149 +34,139 @@ public class RestProcessUtils {
 	private static final String JS_LIBS_PORTABLE_FILE_NAME = "js-portable.html";
 	private static final ServiceConfiguration config = new ServiceConfiguration();
 
+	public static Response generateViewFromTextarea(String swc, boolean portable)
+			throws IOException {
+		return buildHtmlResponse(generateResponseHtml(swc, portable));
+	}
 
-    public static Response generateViewFromTextarea(String swc, boolean portable) throws IOException {
-        return buildHtmlResponse(generateResponseHtml(swc, portable));
-    }
+	public static Response generateViewFromUrl(String url, boolean portable)
+			throws IOException {
+		URLConnection swcSourceConnection = URI.create(url).toURL()
+				.openConnection();
+		return buildHtmlResponse(generateResponseHtml(
+				getInputStreamAsString(swcSourceConnection.getInputStream()),
+				portable));
+	}
 
-    public static Response generateViewFromUrl(String url, boolean portable) throws IOException {
-        URLConnection swcSourceConnection = URI
-            .create(url).toURL()
-            .openConnection();
-        return  buildHtmlResponse(
-            generateResponseHtml(getInputStreamAsString(swcSourceConnection.getInputStream()), portable)
-        );
-    }
+	public static Response generateViewFromFiles(HttpServletRequest request)
+			throws IOException, FileUploadException {
+		return buildHtmlResponse(generateHtmlFromFiles(request));
+	}
 
-    public static Response generateViewFromFiles(HttpServletRequest request) throws IOException, FileUploadException {
-        return buildHtmlResponse(generateHtmlFromFiles(request));
-    }
-
-
-    public static String generateHtmlFromFiles(
-            HttpServletRequest request) throws FileUploadException, IOException {
+	public static String generateHtmlFromFiles(HttpServletRequest request)
+			throws FileUploadException, IOException {
 		List<FileItem> fileItems = uploadFiles(request);
 		LOGGER.info("files uploaded...");
-        return generateResponseHtml(getInputStreamAsString(getFirstFileItem(fileItems).getInputStream()), isPortable(fileItems));
+		return generateResponseHtml(
+				getInputStreamAsString(getFirstFileItem(fileItems)
+						.getInputStream()), isPortable(fileItems));
 	}
 
-
-
-    private static boolean isPortable(List<FileItem> fileItems) {
-        for (FileItem fileItem: fileItems) {
-            if (fileItem.isFormField() && "portable".equals(fileItem.getFieldName())  ) {
-                return fileItem.getString() != null && Boolean.parseBoolean(fileItem.getString());
-            }
-        }
-        return false;
-    }
-
-
-    public static Response generateThumbnailFromFiles(HttpServletRequest request) throws FileUploadException, IOException {
-
-        //upload files
-        List<FileItem> fileItems = uploadFiles(request);
-        LOGGER.info("files uploaded...");
-        // Get only the 1st item (multiple items is not relevant so far)
-        FileItem item = getFirstFileItem(fileItems);
-
-        return generateThumbnail(item.getInputStream());
-
-    }
-
-    public static Response generateThumbnailFromUrl(String url) throws IOException {
-
-        //get swc from url (input stream)
-        URLConnection swcSourceConnection = URI
-                .create(url).toURL()
-                .openConnection();
-
-        return generateThumbnail(swcSourceConnection.getInputStream());
-
-    }
-
-
-    private static Response generateThumbnail(InputStream inputStream) throws IOException {
-
-        Closer closer = Closer.create();
-        closer.register(inputStream);
-
-
-        URLConnection screenshotConn = null;//, viewConn = null;
-        byte[] bytes = null;
-        try {
-            //screenshot service connection
-            screenshotConn = URI
-                    .create(config.getScreenshotServiceUrl()).toURL()
-                    .openConnection();
-            screenshotConn.setDoOutput(true);
-
-
-            //build response entity directly from .swc inputStream
-            InputStream swcResponseInputStream = closer.register(
-                    new ByteArrayInputStream(
-                            generateResponseHtml(getInputStreamAsString(inputStream), true).getBytes("UTF-8")
-                    )
-           );
-
-            ByteStreams.copy(swcResponseInputStream,
-                    closer.register(screenshotConn.getOutputStream()));
-
-            bytes = ByteStreams.toByteArray(screenshotConn.getInputStream());
-
-        } catch (Throwable e) {
-            throw closer.rethrow(e);
-        } finally {
-            closer.close();
-        }
-
-
-
-
-        // Return the response of the screenshot service
-        return Response
-                .status(Status.OK)
-                .entity(bytes)
-                .type("image/png").build();
-    }
-
-
-    public static String generateResponseHtml(String swc) throws IOException {
-        return generateResponseHtml(swc, false);
-    }
-
-	public static String generateResponseHtml(String swc, boolean portable) throws IOException {
-        //get js libs block
-        String chunk = getResourceAsString(
-                portable ? JS_LIBS_PORTABLE_FILE_NAME : JS_LIBS_LINKED_FILE_NAME
-        );
-        //insert js libs block
-		chunk = getResourceAsString(SWC_VIEW_HTML_TEMPLATE_FILE_NAME)
-                .replace("%JS_LIBS_PLACEHOLDER%", chunk);
-        //repalce other placeholders
-		return chunk.replace("%SWC_CONTENT_PLACEHOLDER%", swc)
-		    .replace("%SWC_SERVICE_PLACEHOLDER%", config.getServiceUrl()
-            );
+	private static boolean isPortable(List<FileItem> fileItems) {
+		for (FileItem fileItem : fileItems) {
+			if (fileItem.isFormField()
+					&& "portable".equals(fileItem.getFieldName())) {
+				return fileItem.getString() != null
+						&& Boolean.parseBoolean(fileItem.getString());
+			}
+		}
+		return false;
 	}
 
+	public static Response generateThumbnailFromFiles(HttpServletRequest request)
+			throws FileUploadException, IOException {
+
+		// upload files
+		List<FileItem> fileItems = uploadFiles(request);
+		LOGGER.info("files uploaded...");
+		// Get only the 1st item (multiple items is not relevant so far)
+		FileItem item = getFirstFileItem(fileItems);
+
+		return generateThumbnail(item.getInputStream());
+
+	}
+
+	public static Response generateThumbnailFromUrl(String url)
+			throws IOException {
+
+		// get swc from url (input stream)
+		URLConnection swcSourceConnection = URI.create(url).toURL()
+				.openConnection();
+
+		return generateThumbnail(swcSourceConnection.getInputStream());
+
+	}
+
+	private static Response generateThumbnail(InputStream inputStream)
+			throws IOException {
+
+		Closer closer = Closer.create();
+		closer.register(inputStream);
+
+		URLConnection screenshotConn = null;// , viewConn = null;
+		byte[] bytes = null;
+		try {
+			// screenshot service connection
+			screenshotConn = URI
+					.create(config.getScreenshotServiceUrl()
+							+ "?useFireFox=true").toURL().openConnection();
+			screenshotConn.setDoOutput(true);
+
+			// build response entity directly from .swc inputStream
+			InputStream swcResponseInputStream = closer
+					.register(new ByteArrayInputStream(generateResponseHtml(
+							getInputStreamAsString(inputStream), true)
+							.getBytes("UTF-8")));
+
+			ByteStreams.copy(swcResponseInputStream,
+					closer.register(screenshotConn.getOutputStream()));
+
+			bytes = ByteStreams.toByteArray(screenshotConn.getInputStream());
+
+		} catch (Throwable e) {
+			throw closer.rethrow(e);
+		} finally {
+			closer.close();
+		}
+
+		// Return the response of the screenshot service
+		return Response.status(Status.OK).entity(bytes).type("image/png")
+				.build();
+	}
+
+	public static String generateResponseHtml(String swc) throws IOException {
+		return generateResponseHtml(swc, false);
+	}
+
+	public static String generateResponseHtml(String swc, boolean portable)
+			throws IOException {
+		// get js libs block
+		String chunk = getResourceAsString(portable ? JS_LIBS_PORTABLE_FILE_NAME
+				: JS_LIBS_LINKED_FILE_NAME);
+		// insert js libs block
+		chunk = getResourceAsString(SWC_VIEW_HTML_TEMPLATE_FILE_NAME).replace(
+				"%JS_LIBS_PLACEHOLDER%", chunk);
+		// repalce other placeholders
+		return chunk.replace("%SWC_CONTENT_PLACEHOLDER%", swc).replace(
+				"%SWC_SERVICE_PLACEHOLDER%", config.getServiceUrl());
+	}
 
 	// Helpers
 
-    public static List<FileItem> uploadFiles(HttpServletRequest request)
-            throws FileUploadException {
-        List<FileItem> items = null;
-        if (ServletFileUpload.isMultipartContent(request)) {
-            ServletContext servletContext = request.getServletContext();
-            File repository = (File) servletContext
-                    .getAttribute(JAVAX_SERVLET_CONTEXT_TEMPDIR);
-            DiskFileItemFactory factory = new DiskFileItemFactory();
-            factory.setRepository(repository);
-            ServletFileUpload fileUpload = new ServletFileUpload(factory);
-            items = fileUpload.parseRequest(request);
-        }
-        return items;
-    }
-
+	public static List<FileItem> uploadFiles(HttpServletRequest request)
+			throws FileUploadException {
+		List<FileItem> items = null;
+		if (ServletFileUpload.isMultipartContent(request)) {
+			ServletContext servletContext = request.getServletContext();
+			File repository = (File) servletContext
+					.getAttribute(JAVAX_SERVLET_CONTEXT_TEMPDIR);
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			factory.setRepository(repository);
+			ServletFileUpload fileUpload = new ServletFileUpload(factory);
+			items = fileUpload.parseRequest(request);
+		}
+		return items;
+	}
 
 	public static Response buildHtmlResponse(String str) {
 		return buildHtmlResponse(str, Status.OK);
@@ -187,44 +177,46 @@ public class RestProcessUtils {
 				.build();
 	}
 
-	private static String getResourceAsString(String fileName) throws IOException {
+	private static String getResourceAsString(String fileName)
+			throws IOException {
 		InputStream stream = new RestProcessUtils().getClass().getClassLoader()
 				.getResourceAsStream(fileName);
 		return getInputStreamAsString(stream);
 	}
 
-	private static String getInputStreamAsString(InputStream stream) throws IOException {
-        Closer closer = Closer.create();
-        closer.register(stream);
-        String string = null;
-        try {
-            string = CharStreams.toString(new InputStreamReader(stream, "UTF-8"));
-        } catch (Throwable e) {
-            closer.rethrow(e);
-        } finally {
-            closer.close();
-        }
-        return string;
+	private static String getInputStreamAsString(InputStream stream)
+			throws IOException {
+		Closer closer = Closer.create();
+		closer.register(stream);
+		String string = null;
+		try {
+			string = CharStreams
+					.toString(new InputStreamReader(stream, "UTF-8"));
+		} catch (Throwable e) {
+			closer.rethrow(e);
+		} finally {
+			closer.close();
+		}
+		return string;
 	}
 
+	// get only first processed file!
+	public static FileItem getFirstFileItem(List<FileItem> fileItems)
+			throws IOException {
+		for (FileItem fileItem : fileItems) {
+			if (fileItem.isFormField()) {
+				LOGGER.info("fileItem.getFieldName():"
+						+ fileItem.getFieldName());
+				LOGGER.info("value:" + fileItem.getString());
+			}
+		}
 
-    // get only first processed file!
-    public static FileItem getFirstFileItem(List<FileItem> fileItems) throws IOException {
-        for (FileItem fileItem : fileItems) {
-            if (fileItem.isFormField()) {
-                LOGGER.info("fileItem.getFieldName():" + fileItem.getFieldName());
-                LOGGER.info("value:" + fileItem.getString());
-            }
-        }
-
-
-        for (FileItem fileItem : fileItems) {
-            if (!fileItem.isFormField()) {
-                return fileItem;
-            }
-        }
-        return null;
-    }
-
+		for (FileItem fileItem : fileItems) {
+			if (!fileItem.isFormField()) {
+				return fileItem;
+			}
+		}
+		return null;
+	}
 
 }
